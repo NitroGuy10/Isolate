fdad1d153ae45f5e033ce79d1ff20a0f55a0ad89
tommyztx <tommyztx@gmail.com>
2021-09-11 19:27:45 -0500

    Measure class part 1

--- src/PracticeAlgorithm/Measure.java
+++ src/PracticeAlgorithm/Measure.java
@@ -1,12 +1,15 @@
 package PracticeAlgorithm;
 import MidiReader.Note;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.DoubleSummaryStatistics;
 
 
 public class Measure
 {
-    public ArrayList<Note> notes;
-    public double rating = 0.0;
+    private ArrayList<Note> notes;
+    private double rating = 0.0;
 
     public Measure(ArrayList<Note> measure)
     {
@@ -18,10 +21,30 @@ public class Measure
 
     private void getRating()
     {
-        rating = 0.0;
+        rating = notes.size();
+
+        List<Integer> pitches = notes.stream().map(Note::getPitch).collect(Collectors.toList());
+        double pitch_num = 0.0;
+
+        for(int i = 0; i < pitches.size() - 1; i++)
+        {
+            pitch_num += pitches.get(i) - pitches.get(i + 1);
+        }
+
+        pitch_num /= pitches.size();
+        rating += pitch_num;
+
+        for(int i = 0; i < pitches.size(); i++)
+        {
+            if((pitches.get(i) + 30) % 60 == 0)
+            {
+                rating += 1;
+            }
+        }
     }
     public static void main(String[] args)
     {
+
     }
 }
 
